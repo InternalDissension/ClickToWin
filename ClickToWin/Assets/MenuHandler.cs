8f3ef2a1bc66ced6175c68b54a53becc88857f1f
Durward Craig Jr <Durward.CraigJr@outlook.com>
2016-10-25 10:58:23 +0300

    Created Main Menu
    Added in a fantasy background
    Created a script for spawning balls
    Created a script for handling menus

---
+++ ClickToWin/Assets/MenuHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+    Canvas canvas;
+    RectTransform canvasSize;
+    Text Title;
+    Image background;
+
+    public void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvasSize = GetComponent<RectTransform>();
+        Title = GetComponentInChildren<Text>();
+        background = GetComponentInChildren<Image>();
+
+        setImageSize();
+    }
+
+    private void setImageSize()
+    {
+        background.rectTransform.sizeDelta = canvasSize.rect.size;
+    }
+
+    private void setTitleSize()
+    {
+
+    }
+
+    public void hideCanvas()
+    {
+        canvas.enabled = false;
+    }
+
+    public void showCanvas()
+    {
+        canvas.enabled = true;
+    }
+}
